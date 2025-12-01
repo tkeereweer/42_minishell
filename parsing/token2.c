2bb096eebd7c97b12af3c8f26381ad502f35d5ef
Maxkirit <maximeturgeon@ymail.com>
2025-12-01 14:32:21 +0100

    fix: off by one error in tokenize_word when filename not in quotes

--- parsing/token2.c
+++ parsing/token2.c
@@ -6,7 +6,7 @@
 /*   By: mturgeon <maxime.p.turgeon@gmail.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/12 21:04:31 by mturgeon          #+#    #+#             */
-/*   Updated: 2025/11/27 14:40:59 by mturgeon         ###   ########.fr       */
+/*   Updated: 2025/12/01 14:30:54 by mturgeon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,11 +51,14 @@ int	tokenize_word(char *line, int *i, char **str, int space)
 	{
 		if (!iterate_over_quotes(line, &j))
 			return (tokenizer_error("unclosed quotes\n"));
-		// *i += 1;
 	}
 	else
+	{
 		while (line[j] && valid_char(&line[j]))    
 			j++;
+	}
+	if (line[j] != '\'' && line[j] !='"')
+		j--;
 	*str = ft_substr(&line[*i], 0, j - *i + 1);
 	if (!*str)
 		return (tokenizer_error("malloc fail\n"));
